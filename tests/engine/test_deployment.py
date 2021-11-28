f6087ce27ecf7e880799c5950bb8ba5e40427a6b
Arjun Desai <arjundd@stanford.edu>
2021-11-27 19:11:51 -0800

    update test deployment model to be based on meddlr for stability

--- tests/engine/test_deployment.py
+++ tests/engine/test_deployment.py
@@ -5,8 +5,8 @@ import skm_tea as st
 
 def test_build_deployment_model():
     model = st.build_deployment_model(
-        cfg_or_file="download://https://drive.google.com/file/d/1DTSfmaGu2X9CpE5qW52ux63QrIs9L0oa/view?usp=sharing",  # noqa: E501
-        weights_file="download://https://drive.google.com/file/d/1no9-COhdT2Ai3yuxXpSYMpE76hbqZTWn/view?usp=sharing",  # noqa: E501
+        cfg_or_file="download://https://drive.google.com/file/d/1BJc_lidyHyZvkFD4pLDfSH2Gp2gWXPww/view?usp=sharing",  # noqa: E501
+        weights_file="download://https://drive.google.com/file/d/1EkSdXtnD_28_pjZeVFD6XtYugU7VM3jo/view?usp=sharing",  # noqa: E501
         force_download=True,
     )
     assert isinstance(model, nn.Module)
