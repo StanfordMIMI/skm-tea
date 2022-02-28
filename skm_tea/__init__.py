037025777132133b68ff459f49ad3729fae9c20b
Arjun Desai <ad12@users.noreply.github.com>
2022-02-28 15:37:37 -0800

    bumpversion: v0.0.1a1 -> v0.0.1 (#13)

--- skm_tea/__init__.py
+++ skm_tea/__init__.py
@@ -2,4 +2,4 @@ from skm_tea.engine.model_zoo import get_model_from_zoo  # noqa: F401
 
 # This line will be programatically read/write by setup.py.
 # Leave them at the bottom of this file and don't touch them.
-__version__ = "0.0.1a1"
+__version__ = "0.0.1"
